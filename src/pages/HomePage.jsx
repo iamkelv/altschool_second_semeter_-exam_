da935f7a87a9ec1633d3b89c8b3fbc109b7d118f
Kelvin Moses <moseskelvin4u@gmail.com>
2022-11-06 16:59:57 -0600

    final exam

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,14 @@
 import { BottomHero } from "../components/BottomHero";
 import { HeroSection } from "../components/HeroeSection";
-
+import { Helmet } from "react-helmet";
 export const HomePage = () => {
   return (
     <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Altschool Exam</title>
+        <meta name="description" content="Welcome to altschool Student Exam " />
+      </Helmet>
       <HeroSection />
       <BottomHero />
     </div>
